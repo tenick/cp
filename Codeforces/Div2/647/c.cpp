26c1f503bfd6ce28e05432e1b93bb8ba0bbd4f27
tenick <usareskenneth22@gmail.com>
2024-10-16 20:12:52 +0800

    solve div2 641 b, 647 c

---
+++ Codeforces/Div2/647/c.cpp
@@ -0,0 +1,76 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+// use pos = 0, if you want to work with 1st bit of given number
+template <typename T> inline bool bit_check(T number, int pos) {
+    return (number >> pos) & (T)1;
+}
+template <typename T> inline void bit_toggle(T& number, int pos) {
+    number ^= ((T)1 << pos);
+}
+template <typename T> inline T bit_clear(T number, int pos) {
+    return number & ~((T)1 << pos);
+}
+template <typename T> inline void bit_set(T& number, int pos) {
+    number |= ((T)1 << pos);
+}
+
+ll cnt(ll& n) {
+    if (n == 1) {
+        n = 0;
+        return 1;
+    }
+    ll prev = 1;
+    ll currpow = 1;
+    while ((1ll << (currpow + 1)) <= n) {
+        prev += prev + currpow + 1;
+        currpow++;
+    }
+    ll toAdd = prev + currpow + 1;
+    n -= (1ll << currpow);
+    return toAdd;
+}
+void solve(){
+    ll n;
+    cin >> n;
+    ll ans = 0;
+    while (n != 0) {
+        ans += cnt(n);
+    }
+    cout << ans << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    //std::cout << std::setprecision(9); // use it for output that requires some precision
+
+    int t=1;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+}
