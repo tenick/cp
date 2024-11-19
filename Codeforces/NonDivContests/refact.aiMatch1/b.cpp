366c172b68ebcbaf454452de352c8cc4ed5627b9
tenick <usareskenneth22@gmail.com>
2024-11-20 02:10:29 +0800

    solve refact.ai match1 b

---
+++ Codeforces/NonDivContests/refact.aiMatch1/b.cpp
@@ -0,0 +1,76 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+// use pos = 0, if you want to work with 1st bit of given number
+template <typename T> inline bool bit_check(T number, int pos) {
+    return (number >> pos) & (T)1;
+}
+template <typename T> inline void bit_toggle(T& number, int pos) {
+    number ^= ((T)1 << pos);
+}
+template <typename T> inline T bit_clear(T number, int pos) {
+    return number & ~((T)1 << pos);
+}
+template <typename T> inline void bit_set(T& number, int pos) {
+    number |= ((T)1 << pos);
+}
+
+void solve(){
+  int n;
+  cin >> n;
+  string s, r;
+  cin >> s;
+  cin >> r;
+  int cnt0 = 0;
+  int cnt1 = 0;
+  for (char c : s) {
+    cnt0 += c == '0';
+    cnt1 += c == '1';
+  }
+  for (char c : r) {
+    if (cnt1 == 0 || cnt0 == 0) {
+      cout << "NO\n";
+      return;
+    }
+    if (c == '0') {
+      cnt1--;
+    }
+    else cnt0--;
+  }
+  cout << "YES\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    //std::cout << std::setprecision(9); // use it for output that requires some precision
+
+    int t=1;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+}
