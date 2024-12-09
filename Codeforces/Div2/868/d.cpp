929648dd4acc41eda29e74a9a01c297c4eb6c6d0
tenick <usareskenneth22@gmail.com>
2024-12-09 21:03:36 +0800

    solve div2 868 d

---
+++ Codeforces/Div2/868/d.cpp
@@ -0,0 +1,108 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+// use pos = 0, if you want to work with 1st bit of given number
+template <typename T> inline bool bit_check(T number, int pos) {
+    return (number >> pos) & (T)1;
+}
+template <typename T> inline void bit_toggle(T& number, int pos) {
+    number ^= ((T)1 << pos);
+}
+template <typename T> inline T bit_clear(T number, int pos) {
+    return number & ~((T)1 << pos);
+}
+template <typename T> inline void bit_set(T& number, int pos) {
+    number |= ((T)1 << pos);
+}
+
+#define ONLINE_JUDGE
+
+void solve(){
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+  #endif
+
+  int n, k;
+  cin >> n >> k;
+  vector<int> preflen(k);
+  for (int& i : preflen) cin >> i;
+  vector<int> uniqcnt(k);
+  for (int& i : uniqcnt) cin >> i;
+  
+  if (uniqcnt[0] > preflen[0]) {
+    cout << "NO\n";
+    return;
+  }
+  string ans;
+  int extraAs = uniqcnt[0] - 3;
+  for (int i = 0; i < uniqcnt[0] - 3; i++) {
+    ans.push_back('a');
+  }
+  vector<char> currchars{'a', 'b', 'c'};
+  int currc = 0;
+  for (int i = 0; i < preflen[0] - extraAs; i++) {
+    ans.push_back(currchars[currc]);
+    currc++;
+    currc %= 3;
+  }
+  int newchari = 3;
+  for (int i = 1; i < k; i++) {
+    int extrauniq = uniqcnt[i] - uniqcnt[i-1];
+    int extraLen = preflen[i] - preflen[i-1];
+    if (extrauniq > extraLen) {
+      cout << "NO\n";
+      return;
+    }
+    if (extrauniq > 0) {
+      currchars[currc] = 'a'+newchari;
+      newchari++;
+      for (int j = 0; j < extrauniq; j++)
+        ans.push_back(currchars[currc]);
+      currc++;
+      currc %= 3;
+    }
+    for (int j = 0; j < extraLen - extrauniq; j++) {
+      ans.push_back(currchars[currc]);
+      currc++;
+      currc %= 3;
+    }
+  }
+  cout << "YES\n" << ans << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    //std::cout << std::setprecision(9); // use it for output that requires some precision
+
+    int t=1;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+}
+
