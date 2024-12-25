c70225573920c2c5d3cad2e4990943584ea8a507
tenick <usareskenneth22@gmail.com>
2024-12-25 20:21:49 +0800

    solve div2 169 c, div4 918 f

---
+++ Codeforces/Div2/169/c.cpp
@@ -0,0 +1,113 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+// use pos = 0, if you want to work with 1st bit of given number
+template <typename T> inline bool bit_check(T number, int pos) {
+    return (number >> pos) & (T)1;
+}
+template <typename T> inline void bit_toggle(T& number, int pos) {
+    number ^= ((T)1 << pos);
+}
+template <typename T> inline T bit_clear(T number, int pos) {
+    return number & ~((T)1 << pos);
+}
+template <typename T> inline void bit_set(T& number, int pos) {
+    number |= ((T)1 << pos);
+}
+
+template <typename T> inline T iceil(T a, T b) {
+  return (a + b - 1) / b;
+}
+
+#define MOD_ANSWER
+#ifdef MOD_ANSWER
+constexpr int MOD = 1e9 + 7;
+#endif
+ll binpow(ll a, ll b) {
+  ll res = 1;
+  while (b > 0) {
+    if (b % 2){
+      res *= a;
+      #ifdef MOD_ANSWER
+      res %= MOD;
+      #endif
+    } 
+    a *= a;
+    #ifdef MOD_ANSWER
+    a %= MOD;
+    #endif
+    b /= 2;
+  }
+  return res;
+}
+
+#define ONLINE_JUDGE
+
+void solve(){
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+  #endif
+
+  int n, q;
+  cin >> n >> q;
+  vector<int> arr(n);
+  for (int& i : arr) cin >> i;
+  vector<int> diffArr(n);
+  for (int i = 0; i < q; i++) {
+    int l, r;
+    cin >> l >> r;
+    diffArr[l-1]++;
+    if (r < n)
+      diffArr[r]--;
+  }
+  vector<int> cntsAt(n);
+  int curr = 0;
+  for (int i = 0; i < n; i++) {
+    curr += diffArr[i];
+    cntsAt[i] = curr;
+  }
+  sort(arr.begin(), arr.end());
+  sort(cntsAt.begin(), cntsAt.end());
+  ll ans = 0;
+  for (int i = 0; i < n; i++) {
+    ans += 1ll * arr[i] * cntsAt[i];
+  }
+  cout << ans << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    //std::cout << std::setprecision(9); // use it for output that requires some precision
+
+    int t=1;
+    // cin >> t;
+    while (t--){
+        solve();
+    }
+}
+
