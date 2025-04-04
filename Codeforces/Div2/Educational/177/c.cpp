f727640cda81ca385cc2327332f64ddc33290fa5
tenick <usareskenneth22@gmail.com>
2025-04-04 23:31:37 +0800

    solve div2 educ 177 a b c

---
+++ Codeforces/Div2/Educational/177/c.cpp
@@ -0,0 +1,114 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+// use pos = 0, if you want to work with 1st bit of given number
+template <typename T> inline bool bit_check(T number, int pos) {
+    return (number >> pos) & (T)1;
+}
+template <typename T> inline void bit_toggle(T& number, int pos) {
+    number ^= ((T)1 << pos);
+}
+template <typename T> inline T bit_clear(T number, int pos) {
+    return number & ~((T)1 << pos);
+}
+template <typename T> inline void bit_set(T& number, int pos) {
+    number |= ((T)1 << pos);
+}
+
+template <typename T> inline T iceil(T a, T b) {
+    return (a + b - 1) / b;
+}
+
+#define MOD_ANSWER
+#ifdef MOD_ANSWER
+constexpr int MOD = 1e9 + 7;
+#endif
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while (b > 0) {
+        if (b % 2){
+            res *= a;
+#ifdef MOD_ANSWER
+            res %= MOD;
+#endif
+        } 
+        a *= a;
+#ifdef MOD_ANSWER
+        a %= MOD;
+#endif
+        b /= 2;
+    }
+    return res;
+}
+
+#define ONLINE_JUDGE
+void solve(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int& i : arr) cin >> i;
+    vector<int> q(n);
+    for (int& i : q) cin >> i;
+    vector<int> visited(n, -1);
+    int currcnt = 0;
+    for (int d : q) {
+        // cycle sort
+        vector<int> inds;
+        inds.push_back(d - 1);
+        while (visited[inds.back()] == -1) {
+            int i = inds.back();
+            visited[i] = 0;
+            inds.push_back(arr[i] - 1);
+        }
+        inds.pop_back();
+        if (!inds.empty()) {
+            currcnt += inds.size();
+            for (int i : inds) {
+                visited[i] = inds.size();
+            }
+        }
+        cout << currcnt << ' ';
+    }
+    cout << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    //std::cout << std::setprecision(9); // use it for output that requires some precision
+
+    int t=1;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+}
+
