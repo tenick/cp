736ee5ffb80c4b9885bad9d253c17e49d66833ba
tenick <usareskenneth22@gmail.com>
2025-06-09 01:26:15 +0800

    solve div3 1029 a b c d (during contest)

---
+++ Codeforces/Div3/1029/d.cpp
@@ -0,0 +1,115 @@
+#include <climits>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+template <typename T> inline bool bit_check(T number, int pos) {
+    return (number >> pos) & (T)1;
+}
+template <typename T> inline void bit_toggle(T& number, int pos) {
+    number ^= ((T)1 << pos);
+}
+template <typename T> inline T bit_clear(T number, int pos) {
+    return number & ~((T)1 << pos);
+}
+template <typename T> inline void bit_set(T& number, int pos) {
+    number |= ((T)1 << pos);
+}
+
+template <typename T> inline T iceil(T a, T b) {
+    return (a + b - 1) / b;
+}
+
+#define MOD_ANSWER
+#ifdef MOD_ANSWER
+constexpr int MOD = 1e9 + 7;
+#endif
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while (b > 0) {
+        if (b % 2){
+            res *= a;
+#ifdef MOD_ANSWER
+            res %= MOD;
+#endif
+        } 
+        a *= a;
+#ifdef MOD_ANSWER
+        a %= MOD;
+#endif
+        b /= 2;
+    }
+    return res;
+}
+
+#define ONLINE_JUDGE
+void solve(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+    int n;
+    cin >> n;
+    vector<ll> arr(n);
+    for (ll& i : arr) cin >> i;
+
+    // let x be no. of 1st operation
+    // let y be no. of 2nd operation
+    ll numy = (ll)n * arr[0] - arr[n-1];
+    ll denom = (ll)n * n - 1;
+
+    if (numy % denom != 0) {
+        cout << "NO\n";
+        return;
+    }
+
+    ll y = numy / denom;
+    ll x = arr[0] - n * y;
+    if (x < 0 || y < 0) {
+        cout << "NO\n";
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        ll val = x * (i+1) + y * (n - i);
+        if (arr[i] != val) {
+            cout << "NO\n";
+            return;
+        }
+    }
+
+    cout << "YES\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int t=1;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+}
