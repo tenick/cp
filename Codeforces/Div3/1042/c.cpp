348772c3153e45ad0a6f84efdf812a184b2c5b01
tenick <usareskenneth22@gmail.com>
2025-09-08 01:17:22 +0800

    solve div3 1042 c e

---
+++ Codeforces/Div3/1042/c.cpp
@@ -0,0 +1,116 @@
+#include <climits>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+// use pos = 0, if you want to work with 1st bit of given number
+template <typename T> inline bool bit_check(T number, int pos) {
+    return (number >> pos) & (T)1;
+}
+template <typename T> inline void bit_toggle(T& number, int pos) {
+    number ^= ((T)1 << pos);
+}
+template <typename T> inline T bit_clear(T number, int pos) {
+    return number & ~((T)1 << pos);
+}
+template <typename T> inline void bit_set(T& number, int pos) {
+    number |= ((T)1 << pos);
+}
+
+template <typename T> inline T iceil(T a, T b) {
+    return (a + b - 1) / b;
+}
+
+#define MOD_ANSWER
+#ifdef MOD_ANSWER
+constexpr int MOD = 1e9 + 7;
+#endif
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while (b > 0) {
+        if (b % 2){
+            res *= a;
+#ifdef MOD_ANSWER
+            res %= MOD;
+#endif
+        } 
+        a *= a;
+#ifdef MOD_ANSWER
+        a %= MOD;
+#endif
+        b /= 2;
+    }
+    return res;
+}
+
+#define ONLINE_JUDGE
+void solve(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+    int n, k;
+    cin >> n >> k;
+    vector<int> s(n);
+    for (int& i : s) cin >> i;
+    vector<int> t(n);
+    for (int& i : t) cin >> i;
+
+    map<int, int> smp;
+    for (int i : s) smp[(i % k > k/2) ? (k - (i % k)) : i % k]++;
+
+    map<int, int> tmp;
+    for (int i : t) tmp[(i % k > k/2) ? (k - (i % k)) : i % k]++;
+
+    vector<pair<int, int>> spmp;
+    for (auto& kvp : smp) spmp.push_back(kvp);
+    vector<pair<int, int>> tpmp;
+    for (auto& kvp : tmp) tpmp.push_back(kvp);
+    if (spmp.size() != tpmp.size()) {
+        cout << "NO\n";
+        return;
+    }
+
+    for (int i = 0; i < spmp.size(); i++) {
+        if (spmp[i] != tpmp[i]) {
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    //std::cout << std::setprecision(9); // use it for output that requires some precision
+
+    int t=1;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+}
+
